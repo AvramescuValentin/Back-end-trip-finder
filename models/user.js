93c438d384c823807736fd694253ecc793fa891e
AvramescuRobert <76249242+AvramescuValentin@users.noreply.github.com>
2022-03-22 23:01:02 +0200

    update user.js

--- models/user.js
+++ models/user.js
@@ -4,19 +4,20 @@ const uniqueValidator = require('mongoose-unique-validator');
 const Schema = mongoose.Schema;
 
 const userSchema = new Schema({
-    firstName: {type: String, required: true},
-    lastName: {type: String, required: true},
-    username: {type: String, required: true, unique: true},
-    email: {type: String, required: true, unique: true},
-    password: {type: String, required: true, minlength: 6},
-    gender: {type: mongoose.Types.ObjectId, ref: 'Gender', required:true},
-    image: {type: String, default: 'https://avpn.asia/wp-content/uploads/2015/05/empty_profile.png'},
-    groups: [{type: mongoose.Types.ObjectId, ref: 'Group'}],
-    location: {type: mongoose.Types.ObjectId, ref: 'Location', required: true},
-    dateOfBirth: {type: Date, required: true},
-    tags: [{type: mongoose.Types.ObjectId, ref: 'Tags'}],
+    firstName: { type: String, required: true },
+    lastName: { type: String, required: true },
+    username: { type: String, required: true, unique: true },
+    email: { type: String, required: true, unique: true },
+    phone: { type: String, required: true, unique: true },
+    password: { type: String, required: true, minlength: 6 },
+    gender: { type: mongoose.Types.ObjectId, ref: 'Gender', required: true },
+    image: { type: String, default: 'https://avpn.asia/wp-content/uploads/2015/05/empty_profile.png' },
+    groups: [{ type: mongoose.Types.ObjectId, ref: 'Group' }],
+    location: { type: mongoose.Types.ObjectId, ref: 'Location', required: true },
+    dateOfBirth: { type: Date, required: true },
+    tags: [{ type: mongoose.Types.ObjectId, ref: 'Tags' }],
     rating: Number,
-    review: [{star: Number, commentary: String}]
+    review: [{ star: Number, commentary: String }]
 });
 
 userSchema.plugin(uniqueValidator);
